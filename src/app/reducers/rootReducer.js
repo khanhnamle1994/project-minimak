a3410e35949b52113ef99f3d3642e597f4c2607d
James Le <le_j6@denison.edu>
2017-08-15 13:12:26 -0700

    Add logic for tracking the currently selected mech

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import entitiesReducer from "./entitiesReducer";
 import tabReducer from "features/tabs/tabReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 import pilotsReducer from "features/pilots/pilotsReducer";
+import mechsReducer from "features/mechs/mechsReducer"
 
 const rootReducer = combineReducers({
     entities : entitiesReducer,
     unitInfo : unitInfoReducer,
     pilots : pilotsReducer,
+    mechs : mechsReducer,
     tabs : tabReducer,
 });
 
